eb7f338b0511656b88dc40d4d7b4a392507c7cf1
emily s <emilywselwood@gmail.com>
2024-02-26 06:17:32 +0000

    include css in the binary so it works outside the repo dir

--- src/convert.rs
+++ src/convert.rs
@@ -1,8 +1,9 @@
 use pulldown_cmark::{html, Options, Parser};
+use std::error::Error;
 use std::fs;
 use std::fs::File;
-use std::io::{self, prelude::*};
-use std::error::Error;
+use std::io::prelude::*;
+use std::path::Path;
 
 pub fn markdown_to_html(input_contents: &str) -> String {
     let options = Options::empty();
@@ -13,6 +14,14 @@ pub fn markdown_to_html(input_contents: &str) -> String {
 }
 
 pub fn write_to_file(html_output: String, output_filename: &str) -> Result<(), Box<dyn Error>> {
+    // If we are in the git repo or someone has provided a css file near their working directory we can use that.
+    // Otherwise use the one included at compile time.
+    let css = if Path::new("styles/style.css").exists() {
+        fs::read_to_string("styles/style.css")?
+    } else {
+        DEFAULT_CSS.to_string()
+    };
+
     let precontent = format!(
         r#"<html lang="en">
         <head>
@@ -25,7 +34,7 @@ pub fn write_to_file(html_output: String, output_filename: &str) -> Result<(), B
         <body>
             <div id='content'>"#,
         output_filename.replace(".md", ""),
-        fs::read_to_string("styles/style.css")?,
+        css,
     );
     let postcontent = r#"
             </div>
@@ -35,3 +44,7 @@ pub fn write_to_file(html_output: String, output_filename: &str) -> Result<(), B
     write!(output_file, "{}{}{}", precontent, html_output, postcontent)?;
     Ok(())
 }
+
+// Include the default css at compile time so it doesn't matter where the binary ends up it will always be able to find
+// this file.
+const DEFAULT_CSS: &str = include_str!("../styles/style.css");
